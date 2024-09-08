0c69e56ef3a9d135cb0fe6442434bbee4f487ea8
Hugo Silva <yhugorocha@gmail.com>
2024-09-07 21:45:46 -0300

    initial commit

---
+++ src/main/java/io/github/yhugorocha/testefullstack/TestefullstackApplication.java
@@ -0,0 +1,13 @@
+package io.github.yhugorocha.testefullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestefullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestefullstackApplication.class, args);
+	}
+
+}
