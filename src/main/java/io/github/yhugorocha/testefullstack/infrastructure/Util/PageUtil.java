b298a8e15511a20907d4c8fdab8e5048d9e32cf5
Hugo Silva <yhugorocha@gmail.com>
2024-09-09 14:22:25 -0300

    feat: add classe Util de paginação.

---
+++ src/main/java/io/github/yhugorocha/testefullstack/infrastructure/Util/PageUtil.java
@@ -0,0 +1,20 @@
+package io.github.yhugorocha.testefullstack.infrastructure.Util;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class PageUtil {
+
+    public Page<?> adicionaPaginacao(List<?> list, Integer pageNumber, Integer pageSize){
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), list.size());
+        List<?> subList = list.subList(start, end);
+
+        return new PageImpl<>(subList, pageable, list.size());
+    }
+}
